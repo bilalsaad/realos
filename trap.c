12174dc3e85b267d0ffd9fee8000254ab6c20966
bilalsaad <abusaadb@post.bgu.ac.il>
2016-03-17 13:57:32 +0200

    added QUANTA field in trapc.c for yielding every QUANTA ticks

--- trap.c
+++ trap.c
@@ -104,7 +104,9 @@ trap(struct trapframe *tf)
 
   // Force process to give up CPU on clock tick.
   // If interrupts were on while locks held, would need to check nlock.
-  if(proc && proc->state == RUNNING && tf->trapno == T_IRQ0+IRQ_TIMER)
+  // We added the QUANTA condition for yielding over!
+  if(proc && proc->state == RUNNING && tf->trapno == T_IRQ0+IRQ_TIMER
+      && (ticks % QUANTA) == 0)
     yield();
 
   // Check if the process has been killed since we yielded
